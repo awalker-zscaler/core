63ee9a63342639d1ad9d5b2238df94107931e55f
awalker-zscaler <126690666+awalker-zscaler@users.noreply.github.com>
2023-03-02 11:10:12 -0500

    Update invoke-header.ps1

--- invoke-header.ps1
+++ invoke-header.ps1
@@ -1,3 +1,3 @@
-Write-Host "`n  ZSCALER " -ForegroundColor Cyan -NoNewline
+Write-Host "`nZSCALER " -ForegroundColor Cyan -NoNewline
 Write-Host " - Public Sector Professional Services - " -NoNewline
-Write-Host " Backup and Restore Utility" -ForegroundColor Red
+Write-Host " Backup and Restore Utility`n`n" -ForegroundColor Red
